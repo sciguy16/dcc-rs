7fa6393915131b3b2f1527724fececdd21ff3062
David Young <david@thedavidyoung.co.uk>
2022-06-14 19:05:48 +0100

    Add length check to serialise function

--- src/packets/mod.rs
+++ src/packets/mod.rs
@@ -20,12 +20,19 @@ pub type Result<T> = core::result::Result<T, Error>;
 
 struct Preamble(BitArr!(for 14, in u8, Msb0));
 
+const MAX_BITS: usize = 43;
 /// Buffer long enough to serialise any common DCC packet into
-pub type SerialiseBuffer = BitArr!(for 43, in u8, Msb0);
+pub type SerialiseBuffer = BitArr!(for MAX_BITS, in u8, Msb0);
 
 /// TODO use this method for all serialisations. Should be less error-prone
 /// than all of the manual bit offsets we implemented in baseline.
 fn serialise(data: &[u8], buf: &mut SerialiseBuffer) -> Result<usize> {
+    // check that the provided data will fit into the buffer
+    let required_bits = 15 + data.len() * 9 + 1;
+    if required_bits > MAX_BITS {
+        return Err(Error::TooLong);
+    }
+
     buf[0..16].copy_from_bitslice([0xff, 0xfe].view_bits::<Msb0>()); // preamble
 
     let mut pos: usize = 15;
